b4a01109efba1a90c76e8c6b158855f6f997f4db
tpe1ga <PedroIvan.TelloFlores@mx.bosch.com>
2023-03-01 18:06:35 -0600

    Add pyinstaller to workflow and requirements.txt

--- stream_line_sender.test.py
+++ stream_line_sender.test.py
@@ -1,18 +1,19 @@
 import unittest
 import stream_line_sender as sls
 import stream_line_sender_constants as slsc
-
+import random
 
 class TypewiseTest(unittest.TestCase):
 
   def test_check_and_send(self):
-    for coolingType in slsc.COOLING_TYPE:
-        self.assertTrue(
-          sls.check_and_send(
-            {'coolingType': coolingType},
-            80
-          ) == True
-        )
+    for x in range(50):
+      random_index = random.randrange(3)
+      self.assertTrue(
+        sls.check_and_send(
+          {'coolingType': slsc.COOLING_TYPE[random_index]},
+          random.randrange(-1, 80)
+        ) == True
+      )
 
 
 if __name__ == '__main__':
